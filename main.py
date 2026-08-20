c3453e2b18c50367709563f99c8cbffd8af5335e
sureshr89 <sureshr89@gmail.com>
2026-08-20 11:53:45 +0530

    Add S1-S5 no-signal diagnostics

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from engine.execution_diagnostics_patch import install as install_execution_diag
 from engine.trade_path_fix import install as install_trade_path_fix
 from engine.diagnostic_consistency_patch import install as install_diagnostic_consistency_patch
 from engine.authoritative_dhan_snapshot_patch import install as install_authoritative_dhan_snapshot_patch
+from engine.strategy_diagnostics_patch import install as install_strategy_diagnostics_patch
 
 install_dhan_retry()
 install_dhan_patch(MasterEngine)
@@ -16,6 +17,7 @@ install_trade_path_fix(MasterEngine)
 install_diagnostic_consistency_patch(MasterEngine)
 install_execution_diagnostics_patch(MasterEngine)
 install_authoritative_dhan_snapshot_patch(MasterEngine)
+install_strategy_diagnostics_patch(MasterEngine)
 TradingBot = MasterEngine
 __all__ = ["TradingBot", "MasterEngine"]
 
