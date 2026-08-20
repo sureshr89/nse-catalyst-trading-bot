a011f162e40ab25cbb60c4fb4c8d6b306797d8c1
sureshr89 <sureshr89@gmail.com>
2026-08-20 20:06:53 +0530

    Add fully audited File 25 clean end-to-end contract tests

---
+++ tests/test_clean_end_to_end_contract.py
@@ -0,0 +1,15 @@
+from strategy.nifty500_price_action_strategies import make_signal
+
+
+def test_valid_buy_signal_reaches_trade_contract():
+    signal = make_signal('S1', 'BUY', 'ABC', 1000, 994, 0.10, 0.20, 1.20, 500, 'clean S1 test', 999, 1000, 10, 5)
+    assert signal is not None
+    assert signal.rr == 1.25
+    assert 1400 <= signal.actual_risk <= 1500
+    assert signal.target > signal.entry > signal.stop_loss
+    assert signal.capital_used <= 250000
+
+
+def test_invalid_sell_market_alignment_is_rejected():
+    signal = make_signal('S1', 'SELL', 'ABC', 1000, 1006, -0.10, 0.20, 1.20, 500, 'bad alignment', 1001, 1000, 10, 5)
+    assert signal is None
