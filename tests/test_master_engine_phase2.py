fd302fd1fbd3b8c6c82b8c464b65f0fd34af279a
sureshr89 <sureshr89@gmail.com>
2026-08-20 19:56:35 +0530

    Add File 24 master engine regression tests

---
+++ tests/test_master_engine_phase2.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from engine.master_engine import MasterEngine
+
+def test_blank_diagnostics_start_blocked():
+    engine=object.__new__(MasterEngine)
+    diag=engine._blank_diag()
+    assert diag['market_data_source']=='DHAN_ONLY'
+    assert diag['trade_path_status']=='BLOCKED'
+    assert diag['market_data_coverage']=='0/500'
+
+def test_incomplete_reference_data_cannot_be_ready():
+    engine=object.__new__(MasterEngine)
+    engine._session_date=None
+    engine.references=pd.DataFrame()
+    engine.sector_map=pd.DataFrame()
+    engine.diagnostics=engine._blank_diag()
+    class U:
+        def get_dataframe(self,refresh=False):
+            return pd.DataFrame({'Symbol':['A']})
+    engine.universe_engine=U()
+    engine._refresh_reference_data(True)
+    assert engine.references.empty
+    assert engine.diagnostics['trade_path_status']=='BLOCKED'
