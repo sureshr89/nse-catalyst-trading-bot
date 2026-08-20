0ef137cd86381698555130c2700e9056216fc3a6
sureshr89 <sureshr89@gmail.com>
2026-08-20 10:52:49 +0530

    Connect transparent execution diagnostics to MasterEngine

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from engine.master_engine import MasterEngine
 from engine.dhan_patch import install as install_dhan_patch
 from engine.stability_patch import install as install_stability_patch, install_dhan_retry
 from engine.live_data_alignment_patch import install as install_live_data_alignment_patch
+from engine.execution_diagnostics_patch import install as install_execution_diagnostics_patch
 
 install_dhan_retry()
 install_dhan_patch(MasterEngine)
 install_stability_patch(MasterEngine)
 install_live_data_alignment_patch(MasterEngine)
+install_execution_diagnostics_patch(MasterEngine)
 TradingBot = MasterEngine
 __all__ = ["TradingBot", "MasterEngine"]
 
