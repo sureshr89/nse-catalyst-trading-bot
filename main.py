3b5c4626bc09b66264052aab895965cf73b03e0b
sureshr89 <sureshr89@gmail.com>
2026-08-20 14:58:14 +0530

    Use clean Dhan-only master engine without legacy patches

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-"""NSE Catalyst root entrypoint for paper-trading engine and Streamlit dashboard."""
+"""NSE Catalyst clean root entrypoint."""
 from engine.master_engine import MasterEngine
-from engine.dhan_patch import install as install_dhan_patch
-from engine.stability_patch import install as install_stability_patch, install_dhan_retry
-from engine.live_data_alignment_patch import install as install_live_data_alignment_patch
-from engine.execution_diagnostics_patch import install as install_execution_diagnostics_patch
-from engine.trade_path_fix import install as install_trade_path_fix
-from engine.diagnostic_consistency_patch import install as install_diagnostic_consistency_patch
-from engine.authoritative_dhan_snapshot_patch import install as install_authoritative_dhan_snapshot_patch
-from engine.strategy_diagnostics_patch import install as install_strategy_diagnostics_patch
-from engine.strategy_sector_count_gate_patch import install as install_strategy_sector_count_gate_patch
 
-install_dhan_retry()
-install_dhan_patch(MasterEngine)
-install_stability_patch(MasterEngine)
-install_live_data_alignment_patch(MasterEngine)
-install_trade_path_fix(MasterEngine)
-install_diagnostic_consistency_patch(MasterEngine)
-install_execution_diagnostics_patch(MasterEngine)
-install_authoritative_dhan_snapshot_patch(MasterEngine)
-install_strategy_diagnostics_patch(MasterEngine)
-install_strategy_sector_count_gate_patch(MasterEngine)
 TradingBot = MasterEngine
 __all__ = ["TradingBot", "MasterEngine"]
 
